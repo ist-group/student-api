1e35b79b4e9e85899055ad4dddd476a24fe87ed5
Jonas Erlandsson <jonas.erlandsson@ist.com>
2023-02-21 12:28:34 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NTg3NQ/spectral.js?branch_slug=master&token=dd1707ab-c235-44d6-b19a-8ba9a063fbd3";
+export default { extends: ruleset };
